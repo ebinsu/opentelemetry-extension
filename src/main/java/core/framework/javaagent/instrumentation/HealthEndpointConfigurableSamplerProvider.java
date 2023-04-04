e39c8b376e0fb61c724f0605448f0c3e041075cd
ebin <ebinsu@chancetop.com>
2023-04-04 14:49:41 +0800

    test

---
+++ src/main/java/core/framework/javaagent/instrumentation/HealthEndpointConfigurableSamplerProvider.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package core.framework.javaagent.instrumentation;
+
+import com.google.auto.service.AutoService;
+import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
+import io.opentelemetry.sdk.autoconfigure.spi.traces.ConfigurableSamplerProvider;
+import io.opentelemetry.sdk.trace.samplers.Sampler;
+
+@AutoService(ConfigurableSamplerProvider.class)
+public class HealthEndpointConfigurableSamplerProvider implements ConfigurableSamplerProvider {
+
+    @Override
+    public Sampler createSampler(ConfigProperties config) {
+        return new HealthEndpointSampler();
+    }
+
+    @Override
+    public String getName() {
+        return "health-endpoint";
+    }
+}
