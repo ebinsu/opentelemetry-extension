d91bd6713869edd8ef6c0e7c462ee1a5d3db9462
ebin <ebinsu@chancetop.com>
2023-04-04 11:30:41 +0800

    test

--- src/main/java/core/framework/javaagent/instrumentation/ServletInitialHandlerInstrumentation.java
+++ src/main/java/core/framework/javaagent/instrumentation/ServletInitialHandlerInstrumentation.java
@@ -40,7 +40,9 @@ public class ServletInitialHandlerInstrumentation implements TypeInstrumentation
         @Advice.OnMethodEnter(suppress = Throwable.class)
         public static void onEnter(@Advice.Argument(value = 0) HttpServerExchange exchange) {
             Span current = Span.current();
+            current.setAttribute("test", "hahahah1");
             if (Objects.nonNull(current)) {
+                current.setAttribute("test", "hahahah2");
                 current.updateName(exchange.getRequestMethod() + " " + exchange.getRequestURI());
             }
         }
