0750e16e908524ecd934282d680bc65544dac4e9
ebin <ebinsu@chancetop.com>
2023-04-04 14:53:18 +0800

    test

--- src/main/java/core/framework/javaagent/instrumentation/HealthEndpointSampler.java
+++ src/main/java/core/framework/javaagent/instrumentation/HealthEndpointSampler.java
@@ -33,7 +33,7 @@ public class HealthEndpointSampler implements Sampler {
             SpanKind spanKind,
             Attributes attributes,
             List<LinkData> parentLinks) {
-        if (spanKind == SpanKind.INTERNAL && name.contains("greeting")) {
+        if (spanKind == SpanKind.INTERNAL && name.contains("/actuator/health")) {
             return SamplingResult.create(SamplingDecision.DROP);
         } else {
             return SamplingResult.create(SamplingDecision.RECORD_AND_SAMPLE);
